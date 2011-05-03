1b3b66e23ab6e7da5ca02010f117f980ca82b6ef
michaellieshoff@gmx.de <michaellieshoff@gmx.de@aba428bb-edef-16d3-7cbd-cf5dc25a70a0>
2011-05-03 09:33:28 +0000

    introducing annotation solver.

---
+++ trunk/jusualsparent/jusuals-core/src/main/java/org/mili/core/annotation/AbstractAnnotationSolver.java
@@ -0,0 +1,57 @@
+/*
+ * AbstractAnnotationSolver.java
+ *
+ * 03.05.2011
+ *
+ * Copyright 2011 Michael Lieshoff
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mili.core.annotation;
+
+import java.lang.annotation.*;
+import java.util.*;
+
+/**
+ * This abstract class is an implementation of interface {@link AnnotationSolver}.
+ *
+ * @param <X> the generic type
+ * @author Michael Lieshoff
+ */
+public abstract class AbstractAnnotationSolver<X> implements AnnotationSolver {
+    private Map<Class<?>, List<AnnotationHandler>> handler = new HashMap<Class<?>, List<AnnotationHandler>>();
+
+    @Override
+    public void solve(Class<?> cls) {
+        Annotation[] annotations = this.getAnnotations(cls);
+        for(Annotation annotation : annotations) {
+            List<AnnotationHandler> list = this.handler.get(annotation.annotationType());
+            if (list != null) {
+                for (int i = 0, n = list.size(); i < n; i++) {
+                    AnnotationHandler ah = list.get(i);
+                    ah.handle(annotation);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void addAnnotationHandler(Class<?> forAnnotation, AnnotationHandler ah) {
+        List<AnnotationHandler> list = this.handler.get(ah);
+        if (list == null) {
+            list = new ArrayList<AnnotationHandler>();
+            this.handler.put(forAnnotation, list);
+        }
+        list.add(ah);
+    }
+}
