16542c0824659a1c70113d0ef7f51cfc71a5b5b8
michaellieshoff@gmx.de <michaellieshoff@gmx.de@aba428bb-edef-16d3-7cbd-cf5dc25a70a0>
2011-08-02 10:24:06 +0000

    added experimental text table annotations.

---
+++ jusualsparent/jusuals-core/jusuals-core-impl/src/main/java/org/mili/core/text/annotation/Format.java
@@ -0,0 +1,47 @@
+/*
+ * Format.java
+ *
+ * 01.08.2011
+ *
+ * Copyright 2011 Michael Lieshoff
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.mili.core.text.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * This annotations defines a format.
+ *
+ * @author Michael Lieshoff
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Format {
+
+    /**
+     * Name.
+     *
+     * @return the string
+     */
+    public String name();
+
+    /**
+     * Type.
+     *
+     * @return the format type
+     */
+    public FormatType type();
+}
